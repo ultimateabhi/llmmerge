eaa27c6cfc2a4e1a47bb0c12789b79a2bda94a74
ultimateabhi <ultimateabhi@gmail.com>
2025-05-29 15:12:01 +0530

    fixed installation settings files
    Signed-off-by: ultimateabhi <ultimateabhi@gmail.com>

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="llmmerge",
-    version="0.2.0",
-    packages=find_packages(),
-    install_requires=[],  # Add dependencies here if needed
-    entry_points={
-        "console_scripts": [
-            "llmmerge-driver = llmmerge.driver:main"
-        ]
-    },
-    author="Abhishek Agarwal",
-    email="ultimateabhi@gmail.com",
-    description="A custom LLM-based git merge driver.",
-    python_requires=">=3.10",
-)
+setup()
